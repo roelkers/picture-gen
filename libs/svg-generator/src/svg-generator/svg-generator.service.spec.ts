c904479943d693e2754bf931390c53b99f0f2c44
rufus <oelkersr@media-saturn.com>
2022-08-28 18:05:39 +0200

    adds svg builder

--- libs/svg-generator/src/svg-generator/svg-generator.service.spec.ts
+++ libs/svg-generator/src/svg-generator/svg-generator.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SvgGeneratorService } from './svg-generator.service';
+import { SVG } from './svg.class'
 
 describe('SvgGeneratorService', () => {
   let service: SvgGeneratorService;
@@ -12,7 +13,20 @@ describe('SvgGeneratorService', () => {
     service = module.get<SvgGeneratorService>(SvgGeneratorService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  it('SVG should have correct format', () => {
+    const svg = new SVG(100, 200)
+    .line(100, 200 ,50, 70, "green")
+    .circle(50, 70, 75, 'red')
+    .end()
+    const expected = 
+    `<svg version="1.1"
+         width="100" height="200"
+         xmlns="http://www.w3.org/2000/svg"
+       />
+<line x1="100" y1="200" x2="50" y2="70" fill="green" />
+<circle cx="50" cy="70" r="75" fill="red" />
+</svg>
+`
+    expect(svg.src).toEqual(expected);
+  })
 });
---
+++ libs/svg-generator/src/svg-generator/svg.class.ts
@@ -0,0 +1,60 @@
+export class SVG {
+   
+  public src = ''
+
+  constructor(width: number, height: number) {
+    this.concat(
+      `<svg version="1.1"
+         width="${width}" height="${height}"
+         xmlns="http://www.w3.org/2000/svg"
+       />\n`
+    )
+  }
+
+  private concat(s: string) {
+    this.src = this.src.concat(s)
+  }
+
+  circle(cx: number, cy: number, radius: number, fill?: string) {
+    this.concat(
+      `<circle cx="${cx}" cy="${cy}" r="${radius}" fill="${fill}" />\n`
+    ) 
+    return this
+  }
+
+  rect(width: number, height: number, fill?: string) {
+    this.concat(
+      `<rect width="${width}" height="${height}" fill="${fill}" />\n`
+    ) 
+    return this
+  }
+
+  line(x1: number, y1: number, x2: number, y2: number, fill: string) {
+    this.concat(
+      `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" fill="${fill}" />\n`
+    ) 
+    return this
+  }
+
+  polygon(points: number[], fill: string) {
+    this.concat(
+      `<polygon points="${points.map(p => String(p)).reduce((acc, n) => acc.concat(`${acc},${n}`))}" fill="${fill}"/>\n`
+    ) 
+    return this
+
+  }
+
+  text(x:number, y: number, characters: string) {
+    this.concat(
+      `<text x="${x}" y="${y}" >${characters}</text>\n`
+    ) 
+    return this
+  }
+  
+  end() {
+    this.concat(
+      `</svg>\n`
+    )
+    return this
+  }
+}
